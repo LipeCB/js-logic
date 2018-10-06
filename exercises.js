c4e76509b727fbbbc7a6050631c239f4c783a426
unknown <lipe.casey@gmail.com>
2018-10-05 21:38:09 -1000

    completed exercise 2

--- exercises.js
+++ exercises.js
@@ -49,6 +49,15 @@ console.log('test');
  * The function will return the message: "Login Success!", if the string passed into the function is "test1234"
  * Console.log your result.
 */
+function login(password){
+  if(password === 'test1234'){
+    return 'Login Success';
+
+  }
+
+}
+
+console.log(login('test1234'))
 
 
 /*
